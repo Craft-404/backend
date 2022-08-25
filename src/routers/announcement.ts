d59cb28f7546964c5250badbe66e3da6ee2b2b47
deepg7 <deepgandhi151@gmail.com>
2022-08-26 01:11:19 +0530

    added auth

--- src/routers/announcement.ts
+++ src/routers/announcement.ts
@@ -7,10 +7,14 @@ import {
   RESOURCE_CREATED,
   RESTRICTED_ERROR,
 } from "../middlewares/constants";
+import authFunction from "../middlewares/auth";
 const router = Router();
+
+router.use(authFunction);
 
 router.post("/", async (req: Request, res: Response) => {
   try {
+    console.log(req);
     const designation = await DesignationModel.findById(
       req.employee.designationId
     );
@@ -20,6 +24,7 @@ router.post("/", async (req: Request, res: Response) => {
     await announcement.save();
     return res.status(RESOURCE_CREATED.status).send(announcement);
   } catch (e: any) {
+    console.log(e);
     if (e.status) return res.status(e.status).send(e.message);
     else return res.status(INTERNAL_SERVER_ERROR.status).send(e);
   }
