a1f5b56d3745b6d9e7d248900590721ddc9c6986
deepg7 <deepgandhi151@gmail.com>
2022-08-26 12:14:18 +0530

    designation try catch

--- src/routers/designation.ts
+++ src/routers/designation.ts
@@ -1,14 +1,20 @@
 import { Router, Request, Response } from "express";
 import authFunction from "../middlewares/auth";
+import { INTERNAL_SERVER_ERROR } from "../middlewares/constants";
 import { DesignationModel } from "../models/designation";
 const router = Router();
 
 router.use(authFunction);
 
 router.get("/", async (req: Request, res: Response) => {
-  const designations = await DesignationModel.find({});
-  console.log(designations);
-  return res.send(200).send(designations);
+  try {
+    const designations = await DesignationModel.find({});
+    console.log(designations);
+    return res.send(200).send(designations);
+  } catch (e: any) {
+    if (e.status) return res.status(e.status).send(e);
+    else return res.status(INTERNAL_SERVER_ERROR.status).send(e);
+  }
 });
 
 export default router;
